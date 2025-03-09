12174b4ba0ccf3d2ee896f6bd5ff4e1652e25d05
riley.le <riley.le@avepoint.com>
2025-03-09 22:36:46 +0700

    add infra DI

---
+++ src/Services/Product.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Product.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionstring = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ProductDBContext>((sp, options) =>
+        {
+            options.UseSqlServer(connectionstring);
+        });
+
+        return services;
+    }
+}
