7b7a46c8653c6deb02a31822b177c9ccb7e0212e
Lê Thành An <lethanhan19062001@gmail.com>
2025-08-03 17:24:28 +0700

    addproductapipj

---
+++ src/Services/Product/Product.Grpc/Services/GetProductInfoService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using MediatR;
+using Product.Application.Modules.Queries.GetProductById;
+
+namespace Product.Grpc.Services;
+
+public class GetProductInfoService(IMediator mediator) : Product.ProductBase
+{
+    public override async Task<ProductInfo> GetProductInfo(ProductRequest request, ServerCallContext context)
+    {
+        //var product = await _productRepository.GetProductInfoById(request.Id);
+        var product = await mediator.Send(new GetProductByIdQuery(Guid.Parse(request.Id)));
+        if (product == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, "Product not found"));
+        }
+
+        return new ProductInfo
+        {
+            Price = product.Price.ToString(),
+            Name = product.Name,
+            ImageUrl = product.ImageUrl,
+            Description = product.Description,
+        };
+    }
+}
