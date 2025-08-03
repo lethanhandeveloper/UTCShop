1ebcadf434b060e7746ad0281225db614c131c06
Lê Thành An <lethanhan19062001@gmail.com>
2025-08-03 21:07:54 +0700

    revertgrpprogram

--- src/Services/Product/Product.Grpc/Program.cs
+++ src/Services/Product/Product.Grpc/Program.cs
@@ -1,50 +1,33 @@
-//using BuildingBlocks.BaseDBDataAccess.Interceptors;
-//using BuildingBlocks.Messaging.MassTransit;
-//using BuildingBlocks.Services;
-//using Product.Application;
-//using Product.Grpc.Services;
-//using Product.Infrastructure;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.Configuration.SetAppSettingLocation(AppContext.BaseDirectory, builder.Environment);
-//builder.AddServiceDefaults();
-
-//// Add services to the container.
-//builder.Services.AddGrpc();
-//builder.Services.AddScoped<AuditingSaveChangesInterceptor>();
-//builder.Services.AddApplicationServices(builder.Configuration).AddInfrastructureServices(builder.Configuration);
-//builder.Services.AddMessageBroker(builder.Configuration);
-
-//var app = builder.Build();
-
-
-//app.MapDefaultEndpoints();
-
-//// Configure the HTTP request pipeline.
-//app.MapGrpcService<GetProductInfoService>();
-//app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-//app.Run();
-
-
-
-using ServerTest.Services;
+using BuildingBlocks.BaseDBDataAccess.Interceptors;
+using BuildingBlocks.Messaging.MassTransit;
+using BuildingBlocks.Services;
+using Product.Application;
+using Product.Grpc.Services;
+using Product.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.SetAppSettingLocation(AppContext.BaseDirectory, builder.Environment);
+builder.AddServiceDefaults();
+
 // Add services to the container.
 builder.Services.AddGrpc();
-
+builder.Services.AddScoped<AuditingSaveChangesInterceptor>();
+builder.Services.AddApplicationServices(builder.Configuration).AddInfrastructureServices(builder.Configuration);
+builder.Services.AddMessageBroker(builder.Configuration);
 
 var app = builder.Build();
 
+
+app.MapDefaultEndpoints();
+
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
-
-app.MapGrpcService<GreeterService>();
-
+app.MapGrpcService<GetProductInfoService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+
+
+
 
