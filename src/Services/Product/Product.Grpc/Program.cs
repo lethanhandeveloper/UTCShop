cc45af1d33169a5c698115e35be993152f048d25
Lê Thành An <65018347+lethanhandeveloper@users.noreply.github.com>
2025-08-03 17:29:14 +0700

    Update Program.cs

--- src/Services/Product/Product.Grpc/Program.cs
+++ src/Services/Product/Product.Grpc/Program.cs
@@ -21,8 +21,8 @@ var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GetProductInfoService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
